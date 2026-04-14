e6298e3e36f121eeabefb5d0d67724ca2a540c53
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-04-14 09:47:29 -0500

    detected an rgb array for all 24 patches

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import rawpy
 import matplotlib.pyplot as plt
+import colour
+import colour_checker_detection as ccd
 
 
 sony_img = "./imgs/AKG07755.ARW"
@@ -16,4 +18,23 @@ def display_arw_image(file_path):
     plt.axis('off')
     plt.show()
 
-display_arw_image(sony_img)
+
+def detect_patches(file_path): 
+    with rawpy.imread(file_path) as raw: 
+        rgb = raw.postprocess(output_bps=16) # 16-bit for better precision
+
+    # normalize to 0-1 float
+    image = rgb.astype(np.float32) / 65535.0
+
+    # detect the color checker
+    swatches = ccd.detect_colour_checkers_segmentation(image)
+
+    print(f'Found {len(swatches)} patches')
+    print(swatches)
+
+detect_patches(sony_img)
+
+
+
+
+# display_arw_image(sony_img)
