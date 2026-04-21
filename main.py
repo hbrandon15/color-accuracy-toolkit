9291be26ec97b7ee5efea0c26838a2bf4b78d09e
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-04-21 11:23:24 -0500

    Clarify comments in functions. Import colour and get the reference for ColorChecker24

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import rawpy
 import matplotlib.pyplot as plt
 import colour_checker_detection as ccd
+import colour
 
 
 sony_img = "./imgs/AKG07755.ARW"
@@ -9,12 +10,13 @@ sony_img = "./imgs/AKG07755.ARW"
 
 def display_arw_image(file_path):
     """
-    NOTE: This is intentially using defaults for quick preview. 
+    !!NOTE!!: This is intentially using defaults for quick preview. 
+
     Converting and displaying sensor data into a standard RGB image using rawpy using default settings. 
 
     """
     with rawpy.imread(file_path) as raw:
-        rgb = raw.postprocess() 
+        rgb = raw.postprocess()
     plt.imshow(rgb)
     plt.title('Color Chart ARW Image')
     plt.axis('off')
@@ -32,7 +34,7 @@ def detect_patches(file_path):
                               no_auto_bright=True,
                               use_camera_wb=True,
                               gamma=(1, 1),  # linear -- no gamma curve applied
-                              output_color=rawpy.ColorSpace.sRGB)
+                              output_color=rawpy.ColorSpace.sRGB) # set the output color space
 
     # normalize to 0-1 float
     image = rgb.astype(np.float32) / 65535.0
@@ -46,5 +48,8 @@ def detect_patches(file_path):
 
 detect_patches(sony_img)
 
+print()
+cc = colour.CCS_COLOURCHECKERS['ColorChecker24 - After November 2014']
+print(cc.data)
 
 # display_arw_image(sony_img)
