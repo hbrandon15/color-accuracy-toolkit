77bac3482a8083616df2cd6f07b2a56fb039bb3d
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-04-30 15:11:25 -0500

    correct indentation for plot gamut function

--- main.py
+++ main.py
@@ -175,23 +175,23 @@ def visualize_swatches(image, checker_crop, RGB_reference, RGB_corrected, delta_
     plt.savefig('./delta_e_comparison.png', dpi=150, bbox_inches='tight')
     plt.show()
 
-    def plot_gamut(RGB_reference, RGB_corrected):
+def plot_gamut(RGB_reference, RGB_corrected):
 
-        sRGB = colour.RGB_COLOURSPACES['sRGB']
+    sRGB = colour.RGB_COLOURSPACES['sRGB']
 
-        # convert corrected values to XYZ from sRGB color space
-        # convert corrected values to xy from XYZ
-        RGB_corrected_XYZ = colour.RGB_to_XYZ(RGB_corrected, sRGB, apply_cctf_decoding=False)
-        RGB_corrected_xy = colour.XYZ_to_xy(RGB_corrected_XYZ)
+    # convert corrected values to XYZ from sRGB color space
+    # convert corrected values to xy from XYZ
+    RGB_corrected_XYZ = colour.RGB_to_XYZ(RGB_corrected, sRGB, apply_cctf_decoding=False)
+    RGB_corrected_xy = colour.XYZ_to_xy(RGB_corrected_XYZ)
+    
+    
+
+    # convert corrected values to XYZ from sRGB color space
+    # convert corrected values to xy from XYZ
+    RGB_reference_XYZ = colour.RGB_to_XYZ(RGB_reference, sRGB, apply_cctf_decoding=False)
+    RGB_reference_xy = colour.XYZ_to_xy(RGB_reference_XYZ)
+
        
-       
-
-        # convert corrected values to XYZ from sRGB color space
-        # convert corrected values to xy from XYZ
-        RGB_reference_XYZ = colour.RGB_to_XYZ(RGB_reference, sRGB, apply_cctf_decoding=False)
-        RGB_reference_xy = colour.XYZ_to_xy(RGB_reference_XYZ)
-
-        return None
 
 
 if __name__ == '__main__':
