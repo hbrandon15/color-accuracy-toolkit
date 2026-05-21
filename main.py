9eb28ea37926cef58f402362f4cf605e57174dd4
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-05-21 14:43:31 -0500

    add legend to plot

--- main.py
+++ main.py
@@ -228,6 +228,7 @@ def compare_cameras(iphone_delta_e, sony_delta_e):
 
     ax.bar(x - width/2, sony_delta_e, width, label='Sony a7IV')
     ax.bar(x + width/2, iphone_delta_e, width, label='iPhone 13 Pro Max') 
+    ax.legend()
 
     return None
 
